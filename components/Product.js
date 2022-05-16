355110995e541adf3a58362b56bb71bd5d3d0686
birdboy307 <birdboy307@gmail.com>
2022-05-16 21:18:20 +1000

    initial

--- components/Product.js
+++ components/Product.js
@@ -6,12 +6,12 @@ export default function Product(props) {
     <>
     <div class="bg-white rounded-lg shadow-lg m-10 w-96">
       <div className="">
-                <img src={props.productimage} alt="" class="rounded-t-lg translate-x-8 object-scale-down h-64 w-80"/>
+        <img src={props.productimage} alt="" className="rounded-t-lg translate-x-8 object-scale-down h-64 w-80"/>
       </div>
         <div className="p-6">
           <h2 className="font-bold mb-2 text-2xl text-blue-800">{props.productname}</h2>
           <p className="text-blue-700 mb-2">${props.productprice}</p>
-          <a href="#" class="text-blue-600 hover:text-blue-500 underline text-sm">Add To Cart</a>
+          <a href="#" className="text-blue-600 hover:text-blue-500 underline text-sm">Add To Cart</a>
         </div>
       </div>
     </>
